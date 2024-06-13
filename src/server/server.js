a78c6b141397814799c476676742094044075f38
adinrama <adinrama@student.ub.ac.id>
2024-06-13 07:12:49 +0000

    create predict using ML model

--- src/server/server.js
+++ src/server/server.js
@@ -1,47 +1,55 @@
+require('dotenv').config()
+
 const Hapi = require('@hapi/hapi')
 const routes = require('../server/routes')
+const loadModel = require('../services/loadModel')
 
-  (async () => {
-    const server = Hapi.server({
-      port: 5000,
-      host: 'localhost',
-      routes: {
-        cors: {
-          origin: ['*'],
-        },
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
       },
-    })
+    },
+  })
 
-    server.route(routes)
+  const model = await loadModel()
+  server.app.model = model
 
-    server.ext('onPreResponse', function (request, h) {
-      // Simpan segala response dari setiap request pengguna ke variabel 'response'
-      const response = request.response
+  server.route(routes)
 
-      // Penanganan error jika terjadi kesalahan input (Input Error)
-      if (response instanceof InputError) {
-        const newResponse = h.response({
-          status: 'fail',
-          message: `${response.message} Silakan gunakan foto lain.`
-        })
-        newResponse.code(response.statusCode)
-        return newResponse
-      }
+  server.ext('onPreResponse', function (request, h) {
+    // Simpan segala response dari setiap request pengguna ke variabel 'response'
+    const response = request.response
 
-      // Penanganan error jika terjadi kesalahan pada server (Server Error)
-      if (response.isBoom) {
-        const newResponse = h.response({
-          status: 'fail',
-          message: response.message
-        })
-        newResponse.code(response.statusCode)
-        return newResponse
-      }
+    // Penanganan error jika terjadi kesalahan input (Input Error)
+    // if (response instanceof InputError) {
+    //   const newResponse = h.response({
+    //     status: 'fail',
+    //     message: `${response.message} Silakan gunakan foto lain.`
+    //   })
+    //   newResponse.code(response.statusCode)
+    //   return newResponse
+    // }
 
-      // Program akan melanjutkan eksekusinya jika tidak terdapat error
-      return h.continue
-    })
+    // Penanganan error jika terjadi kesalahan pada server (Server Error)
+    if (response.isBoom) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message
+      })
+      newResponse.code(response.statusCode)
+      return newResponse
+    }
 
-    await server.start()
-    console.log(`Server start at: ${server.info.uri}`)
-  })()
+    // Program akan melanjutkan eksekusinya jika tidak terdapat error
+    return h.continue
+  })
+
+  await server.start()
+  console.log(`Server start at: ${server.info.uri}`)
+}
+
+init()
