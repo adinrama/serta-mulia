8022be4c00209f06d413bcc378b9965d3d9aa5ec
adinrama <adinrama@student.ub.ac.id>
2024-06-13 05:16:47 +0000

    server init

---
+++ src/server/server.js
@@ -0,0 +1,47 @@
+const Hapi = require('@hapi/hapi')
+const routes = require('../server/routes')
+
+  (async () => {
+    const server = Hapi.server({
+      port: 5000,
+      host: 'localhost',
+      routes: {
+        cors: {
+          origin: ['*'],
+        },
+      },
+    })
+
+    server.route(routes)
+
+    server.ext('onPreResponse', function (request, h) {
+      // Simpan segala response dari setiap request pengguna ke variabel 'response'
+      const response = request.response
+
+      // Penanganan error jika terjadi kesalahan input (Input Error)
+      if (response instanceof InputError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: `${response.message} Silakan gunakan foto lain.`
+        })
+        newResponse.code(response.statusCode)
+        return newResponse
+      }
+
+      // Penanganan error jika terjadi kesalahan pada server (Server Error)
+      if (response.isBoom) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message
+        })
+        newResponse.code(response.statusCode)
+        return newResponse
+      }
+
+      // Program akan melanjutkan eksekusinya jika tidak terdapat error
+      return h.continue
+    })
+
+    await server.start()
+    console.log(`Server start at: ${server.info.uri}`)
+  })()
